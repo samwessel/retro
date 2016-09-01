8c7fadd0d6b3670bca779496010f8972d3f1c30e
Sam Wessel <development+samwessel@esendex.com>
2016-09-01 17:32:40 +0100

    Moves client content into public folder

--- public/client.js
+++ public/client.js
@@ -1,41 +1,25 @@
+$(function(){
+	$('form').on('submit',function(e){
+		e.preventDefault();
+		var text = $("#description").val();
+		if(text == ''){
+			return;
+		}
+		var json = JSON.stringify({text });
+		console.log(json);
+		socket.send(json);
+		$("#description").val("");
+	})
+});
 
 var socket = new WebSocket('ws://localhost:8081/');
 socket.onopen = function(event) {
-  log('Opened connection 🎉');
-  var json = JSON.stringify({ message: 'Hello' });
-  socket.send(json);
-  log('Sent: ' + json);
-}
-
-socket.onerror = function(event) {
-  log('Error: ' + JSON.stringify(event));
+  
 }
 
 socket.onmessage = function (event) {
-  log('Received: ' + event.data);
+	var json = event.data;
+	var obj = JSON.parse(json);
+	var message = JSON.parse(obj.message)
+  $("ul").append($("<li>").text(message.text));
 }
-
-socket.onclose = function(event) {
-  log('Closed connection 😱');
-}
-
-document.querySelector('#close').addEventListener('click', function(event) {
-  socket.close();
-  log('Closed connection 😱');
-});
-
-document.querySelector('#send').addEventListener('click', function(event) {
-  var json = JSON.stringify({ message: 'Hey there' });
-  socket.send(json);
-  log('Sent: ' + json);
-});
-
-var log = function(text) {
-  var li = document.createElement('li');
-  li.innerHTML = text;
-  document.getElementById('log').appendChild(li);
-}
-
-window.addEventListener('beforeunload', function() {
-  socket.close();
-});
