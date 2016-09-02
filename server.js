c7235b4fbe262130cf80b068185350ab3c1b4dbe
Sam Wessel <development+samwessel@esendex.com>
2016-09-02 02:23:22 +0100

    Gah

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var showResults = false
 
 var cards = [];
 
-var wss = new WSS({ port: process.env.PORT });
+var wss = new WSS({ port: 8081 });
 wss.on('connection', function(socket) {
 
   for(var i=0; i<cards.length; i++) {
